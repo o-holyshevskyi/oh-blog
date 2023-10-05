bf0f0270dc70b4a1f4605f655aec06ff6c17ac40
oholyshevskyi <oleksandr.holyshevskyi@pcmicorp.com>
2023-10-05 14:16:01 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, { siteTitle, name } from '../components/layout';
+import Date from '../components/date';
+import utilStyles from '../styles/utils.module.css';
+import { getStoredPostsData } from '../lib/posts';
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hi, my name is {name}. I am a Test Automation Engineer at AdvantISS.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getStoredPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
