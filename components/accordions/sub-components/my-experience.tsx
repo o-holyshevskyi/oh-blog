cd1360de8c2a3f028aac0570e305dc7bce9f172c
oholyshevskyi <oleksandr.holyshevskyi@pcmicorp.com>
2023-10-08 11:56:58 +0300

    add new sections for about me page

--- components/accordions/sub-components/my-experience.tsx
+++ components/accordions/sub-components/my-experience.tsx
@@ -1,15 +1,30 @@
+import getCurrentDate from "../../../lib/date";
 import Date from "../../date/date";
+import utilStyles from '../../../styles/utils.module.css';
 
-export default function MyExperience({
-    experience 
-}: { 
-    experience: { 
-        startTime: string; 
-        endTime: string, 
-        companyName: string; 
-        description: string }[] 
-    }) {
-    experience = experience.sort((a, b) => {
+const myExperience = [
+    {
+        startTime: '2019-06-01',
+        endTime: '2021-08-31',
+        companyName: 'TechStudio',
+        description: 'As a QA Engineer on the IPTV project for mobile platforms iOS and Android, I have honed my skills in quality assurance and testing. I have created test cases and other testing artifacts, and have been responsible for testing both the frontend and backend of the application. Additionally, I have taken steps to improve the efficiency of our testing processes by starting to develop automated tests for the API using C#. My experience in this role has also allowed me to share my knowledge and mentor newcomers in the team, contributing to the growth and success of the company.'
+    },
+    {
+        startTime: '2020-02-01',
+        endTime: '2022-07-31',
+        companyName: 'PasynSoft LLC',
+        description: 'Extensive experience on web application projects, I bring a strong technical knowledge to the table. My proficiency in using C# and TypeScript for writing automated tests has allowed me to effectively test and validate software functionality. Additionally, my experience with Selenium and the Playwright library has equipped me with the tools and skills necessary to create comprehensive test suites and ensure the highest level of software quality. In addition to my strong skills in C# and TypeScript, I am also well-versed in a range of other technologies, including JavaScript, NodeJS, SQL, Git, and Azure DevOps. These technical proficiencies have allowed me to effectively design, develop, and execute automated tests for web applications, and have equipped me with the ability to integrate my tests into a larger development workflow.'
+    },
+    {
+        startTime: '2022-08-01',
+        endTime: getCurrentDate(),
+        companyName: 'AdvantISS',
+        description: `As a Test Automation Engineer, I specialize in C# and TypeScript to create automated test scripts using Playwright and Selenium libraries. My strengths include building customized test frameworks tailored to project needs. I've worked on diverse projects, gaining adaptability and in-depth knowledge of software development processes. Additionally, I've mentored junior team members, sharing knowledge and ensuring high-quality results. I excel in essential tasks such as test case design and execution, contributing to project success through technical expertise and attention to detail.`
+    }
+];
+
+export default function MyExperience() {
+    const experience = myExperience.sort((a, b) => {
         if (a.startTime < b.startTime) {
             return 1;
         } else {
@@ -20,11 +35,14 @@ export default function MyExperience({
         <>
             {
                 experience.map((exp, index) => (
-                    <div key={index}>
-                        <Date dateString={exp.startTime}></Date> - <Date dateString={exp.endTime}></Date>
-                        <h2>{exp.companyName}</h2>
-                        <p>{exp.description}</p>
-                    </div>
+                    <>
+                        <div key={index}>
+                            <h2>{exp.companyName}</h2>
+                            <Date dateString={exp.startTime}></Date> - <Date dateString={exp.endTime}></Date>
+                            <p>{exp.description}</p>
+                        </div>
+                        <div className={utilStyles.delimiter}></div>
+                    </>
                 ))
             }
         </>
--- components/accordions/sub-components/my-skills.tsx
+++ components/accordions/sub-components/my-skills.tsx
@@ -2,13 +2,81 @@ import style from './my-skills.module.css';
 import { Popover, PopoverTrigger, PopoverContent } from '@nextui-org/popover';
 import Image from 'next/image';
 
-export default function MySkills({
-    skills
-}: { skills: { 
-    name: string; 
-    description: string;
-    img: string;
-}[] }) {
+
+const skills = [
+    {
+        name: 'C#',
+        description: 'I have honed my expertise in leveraging C# and TypeScript programming languages to create robust and efficient automated test scripts',
+        img: '/images/skills/csharp-50.png'
+    },
+    {
+        name: 'TypeScript',
+        description: 'I have honed my expertise in leveraging C# and TypeScript programming languages to create robust and efficient automated test scripts',
+        img: '/images/skills/typescript-50.png'
+    },
+    {
+        name: 'Playwright',
+        description: 'My work revolves around ensuring the reliability and accuracy of software applications by employing the Playwright and Selenium libraries',
+        img: '/images/skills/theater-masks-50.png'
+    },
+    {
+        name: 'Selenium',
+        description: 'My work revolves around ensuring the reliability and accuracy of software applications by employing the Playwright and Selenium libraries',
+        img: '/images/skills/selenium-50.png'
+    },
+    {
+        name: 'Git',
+        description: 'I effectively use Git for version control, ensuring collaborative development and tracking changes in the test automation codebase',
+        img: '/images/skills/git-50.png'
+    },
+    {
+        name: 'API',
+        description: 'I have hands-on experience with API testing using tools like POSTMAN, enabling thorough verification of RESTful services and API endpoints',
+        img: '/images/skills/rest-api-50.png'
+    },
+    {
+        name: 'Postman',
+        description: 'Postman allows me to create and execute API requests, including GET, POST, PUT, DELETE, and more which is helpful in API testing',
+        img: '/images/skills/postman-api-50.png'
+    },
+    {
+        name: 'SQL',
+        description: 'My role involves working with SQL to perform database testing, validating data integrity, and ensuring that the application interacts correctly with databases',
+        img: '/images/skills/sql-50.png'
+    },
+    {
+        name: 'Node.js',
+        description: 'I have leveraged Node.js to enhance automation scripts and testing frameworks, improving their efficiency and performance',
+        img: '/images/skills/nodejs-50.png'
+    },
+    {
+        name: 'Docker',
+        description: 'I have practical experience with Docker, allowing me to create and manage containerized environments for testing, promoting consistency and repeatability in testing setups',
+        img: '/images/skills/docker-50.png'
+    },
+    {
+        name: 'Scrum',
+        description: 'Worked within Agile Scrum methodologies, contributing to sprint planning, backlog refinement, and daily stand-up meetings for efficient project management',
+        img: '/images/skills/sprint-iteration-50.png'
+    },
+    {
+        name: 'Jira',
+        description: 'Proficient in Jira for tracking and managing project tasks, defects, and user stories, ensuring alignment with project goals',
+        img: '/images/skills/jira-50.png'
+    },
+    {
+        name: 'Static testing',
+        description: 'I have contributed to static testing efforts, identifying and documenting defects in the early stages of development, resulting in reduced software maintenance costs and improved overall quality',
+        img: '/images/skills/testing-50.png'
+    },
+    {
+        name: 'Software testing',
+        description: 'Contributed to the overall improvement of software quality by identifying defects and issues, reducing manual testing efforts, and ensuring the timely delivery of high-quality software products',
+        img: '/images/skills/ab-testing-50.png'
+    },
+]
+
+export default function MySkills() {
     return (
         <div className={style.skills}>
             {skills.map((skill, index) => {
@@ -17,11 +85,7 @@ export default function MySkills({
                         <Popover showArrow={true} placement="bottom" offset={10}>
                             <PopoverTrigger>
                                 <div key={index} className={style.skill}>
-                                    <div 
-                                        style={{textAlign: 'center'}}
-                                    >
-                                        {skill.name}
-                                    </div>
+                                    {skill.name}
                                 </div>
                             </PopoverTrigger>
                             <PopoverContent className={style.popover}>
