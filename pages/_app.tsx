0a4edef112963457948a28dd58bf59da738f3348
oholyshevskyi <oleksandr.holyshevskyi@pcmicorp.com>
2023-10-17 17:39:05 +0300

    add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,17 @@ import '../styles/global.css';
 import '../styles/variables.css';
 import '../styles/syntax-highlighting.css';
 import { ThemeProvider } from '../components/theme-util/theme-context';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Head>
+        <meta name="description" content="Oleksandr Holyshevskyi | Test Automation Engineer" />
+        <meta property="og:title" content="Oleksandr Holyshevskyi" />
+        <meta property="og:description" content="Oleksandr Holyshevskyi | Test Automation Engineer" />
+        <meta property="og:image" content="/images/profile.jpg" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
