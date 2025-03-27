164b0d8fae03cf284f4beb6826b4199491719469
oleksandr.holyshevskyi <oleksandr.holyshevskyi@external.thalesgroup.com>
2025-03-27 11:49:16 +0100

    fix footer

--- app/[locale]/collapsibale-footer.tsx
+++ app/[locale]/collapsibale-footer.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
 import { CardFooter } from "@nextui-org/card";
 import { Button } from "@nextui-org/react";
 
@@ -25,17 +24,8 @@ export default function CollapsibleFooter({ children }: { children: React.ReactN
         <div className="relative items-center justify-start">
             {/* Toggle Button - Only Visible on Mobile */}
             {isMobile && (
-                <div
-                    // animate={{
-                    //     top: isOpen ? "-100px" : "auto",       
-                    //     bottom: isOpen ? "auto" : 10,        
-                    //     right: isOpen ? "3%" : "40%",  
-                    //     translateX: isOpen ? "0%" : "-50%",    
-                    //     rotate: isOpen ? 360 : 0  
-                    // }}
-                    // transition={{ type: "spring", duration: 100, stiffness: 30,  }} // Smooth transition
-                    className="absolute z-20"
-                >
+                <div className={`absolute bottom-4 left-[45%] z-20 transition-all duration-300 ease-in-out 
+                    ${isOpen ? 'rotate-[360deg] translate-y-[-220%] translate-x-[460%]' : ''}`}>
                     <Button
                         className="md:hidden"
                         onClick={() => setIsOpen(!isOpen)}
@@ -48,12 +38,10 @@ export default function CollapsibleFooter({ children }: { children: React.ReactN
                 </div>
             )}
 
-            {/* Footer Content */}
+            {/* Footer Content with Transition and Animation */}
             <div
-                className={`absolute bottom-1 left-1/2 transform -translate-x-1/2 w-[95%] md:w-[calc(100%-16px)] shadow-small rounded-large`}
-                // initial={false} // Avoid hydration mismatch
-                // animate={{ opacity: isOpen ? 1 : 0, height: isOpen ? "auto" : 0 }}
-                // transition={{ duration: 0.3 }}
+                className={`absolute bottom-1 left-1/2 transform -translate-x-1/2 w-[95%] md:w-[calc(100%-16px)] shadow-small rounded-large transition-all duration-300 ease-in-out 
+                ${isOpen ? 'opacity-100 translate-y-0 h-auto' : 'opacity-0 translate-y-10 h-0 pointer-events-none'}`} // Transition added for opacity and translate
             >
                 <CardFooter className="flex flex-col md:flex-row justify-between items-center border-white/20 border-1 py-2 md:py-1">
                     {children}
@@ -65,8 +53,8 @@ export default function CollapsibleFooter({ children }: { children: React.ReactN
 
 const ChevronUp = () => {
     return (
-        <svg 
-            viewBox="0 0 24 24" 
+        <svg
+            viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             width={20}
@@ -75,28 +63,32 @@ const ChevronUp = () => {
             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
             <g id="SVGRepo_iconCarrier">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z" fill="currentColor"></path>
+                <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z"
+                    fill="currentColor"
+                ></path>
             </g>
         </svg>
-    )
-}
-
+    );
+};
 
 const ChevronDown = () => {
     return (
-        <svg 
-            fill="currentColor" 
-            xmlns="http://www.w3.org/2000/svg" 
-            viewBox="0 0 52 52" 
-            enable-background="new 0 0 52 52" 
+        <svg
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 52 52"
+            enable-background="new 0 0 52 52"
             className="text-default-500"
             width={20}
         >
             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-            <g id="SVGRepo_iconCarrier"> 
-                <path d="M47.6,17.8L27.1,38.5c-0.6,0.6-1.6,0.6-2.2,0L4.4,17.8c-0.6-0.6-0.6-1.6,0-2.2l2.2-2.2 c0.6-0.6,1.6-0.6,2.2,0l16.1,16.3c0.6,0.6,1.6,0.6,2.2,0l16.1-16.2c0.6-0.6,1.6-0.6,2.2,0l2.2,2.2C48.1,16.3,48.1,17.2,47.6,17.8z"></path> 
+            <g id="SVGRepo_iconCarrier">
+                <path d="M47.6,17.8L27.1,38.5c-0.6,0.6-1.6,0.6-2.2,0L4.4,17.8c-0.6-0.6-0.6-1.6,0-2.2l2.2-2.2 c0.6-0.6,1.6-0.6,2.2,0l16.1,16.3c0.6,0.6,1.6,0.6,2.2,0l16.1-16.2c0.6-0.6,1.6-0.6,2.2,0l2.2,2.2C48.1,16.3,48.1,17.2,47.6,17.8z"></path>
             </g>
         </svg>
-    )
-}
+    );
+};
